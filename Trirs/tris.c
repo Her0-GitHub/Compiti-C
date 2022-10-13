c0b42afdfaeb39ef4600ee8207f6e42668ef1ed3
Her0_ <79155428+Her0-GitHub@users.noreply.github.com>
2022-10-13 18:47:06 +0200

    Added Tris.c files

--- Trirs/tris.c
+++ Trirs/tris.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+#define DIM 3
+
+int tabella[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+
+void stampa();
+void tab();
+
+int main()
+{
+    stampa();
+}
+
+void stampa()
+{   
+    int r, c;
+
+    for(r=0; r<DIM; r++)
+    {   
+        puts("\t\t|\t\t|");
+        for(c=0; c<DIM; c++)     
+        {
+            if(tabella[r][c]==1)        printf("\tX\t");
+            else if(tabella[r][c]==2)   printf("\tO\t");
+            else                        printf("\t \t");
+            if(c!=DIM-1)                 printf("|");
+        }
+        if(r==DIM-1)    puts("\n\t\t|\t\t|");
+        else            puts("\n\t\t|\t\t|\n-------------------------------------------------");
+    }
+}
+
+void tab()
+{
+    tabella[DIM][DIM] = {{0,0,0},{0,0,0},{0,0,0}};
+}
 
