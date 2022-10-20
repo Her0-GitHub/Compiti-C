fed06d3a078af62aa027400c09e265699d7e678f
her0 <79155428+Her0-GitHub@users.noreply.github.com>
2022-10-20 16:14:29 +0200

    Update 20/10

--- Trirs/tris.c
+++ Trirs/tris.c
@@ -1,26 +1,41 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
+#include <windows.h>
 #define DIM 3
 
-int tabella[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+void  GotoXY(int x, int  y) {
+COORD CursorPos = {x, y};
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+SetConsoleCursorPosition(hConsole, CursorPos);
+}
+
+int tabella[DIM][DIM]={{5,6,7},{8,9,10},{11,12,13}};
 
 void stampa();
-void tab();
+void round(int);
+int controllo(int);
 
 int main()
 {
+    int turno = 4;
+    do{
+        stampa();
+        round(turno);
+        turno++;
+    }while(controllo(turno));
     stampa();
 }
 
 void stampa()
-{   
+{
     int r, c;
-
+    system("cls || clear");
+    GotoXY(0,0);
     for(r=0; r<DIM; r++)
-    {   
+    {
         puts("\t\t|\t\t|");
-        for(c=0; c<DIM; c++)     
+        for(c=0; c<DIM; c++)
         {
             if(tabella[r][c]==1)        printf("\tX\t");
             else if(tabella[r][c]==2)   printf("\tO\t");
@@ -32,8 +47,26 @@ void stampa()
     }
 }
 
-void tab()
+void round(int turno)
 {
-    tabella[DIM][DIM] = {{0,0,0},{0,0,0},{0,0,0}};
+    int r, c;
+    printf("\nTurno Giocatore %d", (turno % 2)+1);
+    do{
+        do{printf("\nInserisci la colonna in cui vuoi mettere il segno: "), scanf("%d", &c);}while(1>c && c>3);
+        do{printf("\nInserisci la riga in cui vuoi mettere il segno: "), scanf("%d", &r);}while(1>r && r>3);
+    }while(tabella[r-1][c-1]==1 || tabella[r-1][c-1]==2);
+    tabella[r-1][c-1] = (turno % 2)+1;
+}
+
+int controllo(int turno)
+{
+    if(turno>=9)
+    {
+        int r, c;
+    if (( ((tabella[0][0]==1) && (tabella[0][1]==1) && (tabella[0][2]==1)) || ((tabella[1][0]==1) && (tabella[1][1]==1) && (tabella[1][2]==1)) || ((tabella[2][0]==1) && (tabella[2][1]==1) && (tabella[2][2]==1)) || ((tabella[0][0]==1) && (tabella[1][1]==1) && (tabella[2][2]==1)) || ((tabella[2][0]==1) && (tabella[1][1]==1) && (tabella[0][2]==1)) ) || ( ((tabella[0][0]==2) && (tabella[0][1]==2) && (tabella[0][2]==2) ) || ((tabella[1][0]==2) && (tabella[1][1]==2) && (tabella[1][2]==2)) || ((tabella[2][0]==2) && (tabella[2][1]==2) && (tabella[2][2]==2)) || (((tabella[0][0]==2) && (tabella[1][1]==2) && (tabella[2][2]==2)) || ((tabella[2][0]==2) && (tabella[1][1]==2) && (tabella[0][2]==2)) ) ))
+        return 0;
+    }
+        else
+        return 1;
 }
 
